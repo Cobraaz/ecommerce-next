5d1b85b88b031c78b9fa9a4f9cfe05b95ca182eb
cobraaz <anujbansal077@gmail.com>
2021-08-28 15:10:18 +0530

    improved the mutation hooks and fetcher improvements

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,15 +1,22 @@
 import { useAddItem } from "@common/cart";
+import { MutationHook } from "@common/types/hooks";
 
 export default useAddItem;
 
-export const handler = {
-  fetcher: () => {
-    console.log("Fetching Data!");
+export const handler: MutationHook = {
+  fetcher: async ({ fetch, input }) => {
+    const response = await fetch({
+      url: "http://localhost:4000/graphql",
+      query: `query { hello }`,
+    });
+
+    return response;
   },
-  useHook: () => {
-    return (input: any) => {
+  useHook: ({ fetch }) => {
+    return async (input: any) => {
+      const response = await fetch(input);
       return {
-        output: JSON.stringify(input) + "_MODIFIED",
+        output: response,
       };
     };
   },
--- framework/shopify/utils/fetch-api.ts
+++ framework/shopify/utils/fetch-api.ts
@@ -1,32 +1,28 @@
-import {
-  ApiFetcherOptions,
-  ApiFetcherResults
-} from "@common/types/api"
+import { ApiFetcherOptions, ApiFetcherResults } from "@common/types/api";
 
 const fetchApi = async <T>({
   url,
   query,
-  variables }: ApiFetcherOptions
-): Promise<ApiFetcherResults<T>> => {
+  variables,
+}: ApiFetcherOptions): Promise<ApiFetcherResults<T>> => {
   const res = await fetch(url, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       query,
-      variables
-    })
-  })
-
-  const { data, errors} = await res.json()
+      variables,
+    }),
+  });
+  const { data, errors } = await res.json();
   // ?? is checking if left hand expression is null or undefined -> if it is go with right expression
   // || is checking if left hand expression is null, undefined, "", 0, false
   if (errors) {
-    throw new Error(errors[0].message ?? errors.message)
+    throw new Error(errors[0].message ?? errors.message);
   }
 
-  return { data }
-}
+  return { data };
+};
 
-export default fetchApi
+export default fetchApi;
